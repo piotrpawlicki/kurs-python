a29efdfa55d2e291bbc064c282569c16af63cfef
piotrpawlicki <piotr.a.pawlicki@gmail.com>
2023-04-20 19:00:10 +0200

    dodanie zmian w zadaniu 2 i w zadaniu 3

--- 01 - zajęcia 2/zadanie2-dyn.py
+++ 01 - zajęcia 2/zadanie2-dyn.py
@@ -1,2 +1,15 @@
-bin_val = input('Enter binary value : ')
-len_bin_val = len(bin_val)
+bin_num = input("Enter binary number: ")
+
+# Sprawdzenie poprawności formatu
+
+while bin_num.isdigit() == False:
+    print("Wrong format!")
+    bin_num = input("Enter binary number: ")
+
+dec_num = 0               ## inicjacja zmiennej dec_num
+power = len(bin_num) - 1  ## wyliczenie maksymalnej potęgi
+
+for digit in bin_num:     ## nie definiuję wcześniej zmiennej digit, bo to zmienna tymczasowa dla pętli for
+    dec_num = dec_num + int(digit) * (2 ** power)
+    power = power - 1
+print("Decimal value:", dec_num)
--- 01 - zajęcia 2/zadanie3.py
+++ 01 - zajęcia 2/zadanie3.py
@@ -1,2 +1,5 @@
-hex_num = 1DB
-oct_num = 2063
+hex_num = 0x1DB
+oct_num = 0o2063
+
+print(hex_num)
+print(oct_num)
