0e9daa1896e3690bd7c6b2b9a2bf7187c17461b5
piotrpawlicki <piotr.a.pawlicki@gmail.com>
2023-04-27 14:36:39 +0200

    Dodanie drugiej wersji dla silni

--- 02 - Przepływy kontrolne/2. Pętla for zadania domowe.py
+++ 02 - Przepływy kontrolne/2. Pętla for zadania domowe.py
@@ -1,9 +1,8 @@
-
 ## zad1
 print('***********************************')
 print('Zad1')
 
-items = ['latarka','zapalniczka','namiot']
+items = ['latarka', 'zapalniczka', 'namiot']
 for i in items:
     print(i)
 print('Great, we are ready!')
@@ -13,7 +12,7 @@ print('Great, we are ready!')
 print('***********************************')
 print('Zad2')
 
-items = ['oliwa','czosnek','chilli','makaron']
+items = ['oliwa', 'czosnek', 'chilli', 'makaron']
 for i in items:
     print(i)
 print('Podawaj na ciepło')
@@ -25,10 +24,10 @@ print('Zad3')
 
 a = 0
 result = []
-for i in range(1,12):
+for i in range(1, 12):
     result.append(str(a))
     a = a + i
-str_result =  ';'.join(result)
+str_result = ';'.join(result)
 print(str_result)
 ########################################################################################
 ## zad4
@@ -36,32 +35,33 @@ print('***********************************')
 print('Zad4')
 
 x = int(input('Wprowadź liczbę w zakresie od 0 do 8: '))
-while x not in range(0,9):
+while x not in range(0, 9):
     x = int(input('Błąd zakresu. Wprowadź liczbę w zakresie od 0 do 8: '))
 if x == 0:
     print('0! = 1')
 else:
     silnia = 1
     liczby = []
-    for i in range(1,x+1):
+    for i in range(1, x + 1):
         silnia *= i
         liczby.append(str(i))
     wynik = ' * '.join(liczby)
     print(f"{x}! =")
     print(f"{wynik} = {silnia}")
 
-
 ########################################################################################
 ## zad4
 print('***********************************')
 print('Zad4 - wersja druga')
 
+
 def silnia(a):
     if a != 0:
-        x = a * silnia(a-1)
+        x: int = a * silnia(a-1)
         return x
     else:
         return 1
+
 
 x = int(input('Wprowadź liczbę w zakresie od 0 do 8: '))
 while x not in range(0, 9):
