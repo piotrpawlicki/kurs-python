4ebf18bc6c50c93096ae5a66e431e7d23049bf8c
piotrpawlicki <piotr.a.pawlicki@gmail.com>
2023-05-30 19:50:19 +0200

    Dodanie zadań z wyjątków

---
+++ 07 - Wyjątki/zad 7/zad 7 - zabezpieczenie plików.py
@@ -0,0 +1,24 @@
+import random
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+#zabezpieczenie plikÓw za pomocą użycia Thinkter
+
+Tk().withdraw()
+file = askopenfilename(filetypes=[("Pliki tekstowe", "*.txt")])
+
+
+def quotes(file):
+    with open(file,'r') as file:
+        for line in file:
+            quote = file.readlines()
+            chosen = random.choice(quote)
+            width = len(chosen)
+            print('Quote of the day is: ')
+            print('*' * width)
+            to_split = chosen.split(' - ')
+            for elem in to_split:
+                print(elem.strip().center(width))
+            print('*' * width)
+
+quotes(file)
---
+++ 07 - Wyjątki/zad 9/zad 9 - wersja 1.py
@@ -0,0 +1,28 @@
+import webbrowser
+from urllib.error import URLError
+
+def get_user_url():
+    user_url = input("Enter a URL: ")
+    return user_url
+
+def check_url(user_url):
+    if user_url.endswith(".pl") or user_url.endswith(".com"):
+        if user_url.startswith("http://") or user_url.startswith("https://"):
+            return True
+        else:
+            user_url = "http://"+user_url
+            return user_url
+    else:
+        raise URLError("Invalid URL")
+       # return False
+
+def main():
+    user_url = get_user_url()
+    try:
+        user_url =  check_url(user_url)
+    except URLError:
+        print("Invalid URL")
+    webbrowser.open(user_url)
+
+if __name__ == "__main__":
+    main()
